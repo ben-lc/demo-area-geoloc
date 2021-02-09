b1c8b6fda141c4a746a1ba5f6d67041b6a522a7c
Benjamin Ledentec <benjamin.ledentec@mnhn.fr>
2021-02-09 18:33:24 +0100

    init a leaflet map centered on france

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,16 @@
 <script>
-  export let name;
+  import Map from "./Map.svelte";
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <Map />
 </main>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""
+  />
+</svelte:head>
---
+++ src/Map.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+  import * as L from "leaflet";
+
+  let map;
+
+  onMount(() => {
+    map = L.map("map").setView([47.41322, -1.319482], 6);
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
+  });
+</script>
+
+<div id="map" />
+
+<style>
+  #map {
+    width: 1080px;
+    height: 720px;
+  }
+</style>
